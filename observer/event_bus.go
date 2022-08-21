49780312b13b2fb84b650bd6eaab03dbebb27608
qyzzy <42561599+qyzzy@users.noreply.github.com>
2022-08-22 00:01:28 +0800

    initial commit event_bus

---
+++ observer/event_bus.go
@@ -0,0 +1,60 @@
+package observer
+
+import (
+	"fmt"
+	"reflect"
+	"sync"
+)
+
+type Bus interface {
+	Subscribe(topic string, handler interface{}) error
+	Publish(topic string, args ...interface{})
+}
+
+type AsyncEventBus struct {
+	handlers map[string][]reflect.Value
+	lock     sync.Mutex
+}
+
+func NewAsyncEventBus() *AsyncEventBus {
+	return &AsyncEventBus{
+		handlers: map[string][]reflect.Value{},
+		lock:     sync.Mutex{},
+	}
+}
+
+func (bus *AsyncEventBus) Subscribe(topic string, f interface{}) error {
+	bus.lock.Lock()
+	defer bus.lock.Unlock()
+
+	v := reflect.ValueOf(f)
+	if v.Type().Kind() != reflect.Func {
+		return fmt.Errorf("handler is not a function")
+	}
+
+	handler, ok := bus.handlers[topic]
+	if !ok {
+		handler = []reflect.Value{}
+	}
+	handler = append(handler, v)
+	bus.handlers[topic] = handler
+
+	return nil
+}
+
+func (bus *AsyncEventBus) Publish(topic string, args ...interface{}) {
+	handlers, ok := bus.handlers[topic]
+	if !ok {
+		fmt.Println("not found handlers in topic:", topic)
+		return
+	}
+
+	params := make([]reflect.Value, len(args))
+	for i, args := range args {
+		params[i] = reflect.ValueOf(args)
+	}
+
+	for i := range handlers {
+		go handlers[i].Call(params)
+	}
+}
